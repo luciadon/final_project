cc47133798f8dc6738d34d0221479f2636bb5ebe
Donskoi <don19q@csiro.au>
2020-03-18 09:34:58 +1000

    Setting my project up

---
+++ Scripts/THE_script.R
@@ -0,0 +1,7 @@
+#This is where I am starting my FINAL project for CSIROs DataSchool Focus Course
+#Not sure what will need to happen yet, but for now let's set it up#
+#Step 1- bringing in my data#
+library(tidyverse)
+library(readxl)
+asldata <- read_xlsx('Data/ASL_DATA_only.xlsx')
+#life is grand!#
